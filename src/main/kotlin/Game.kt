bcedd7e8ab44e4513c0d8a7cb0ba1989d08db756
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-03-01 21:17:32 +0900

    条件式

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -3,19 +3,21 @@ fun main() {
     var healthPoints = 89
     val isBlessed = true
 
-    if (healthPoints == 100) {
-        println("$name is in excellent condition!")
+    val healthStatus = if (healthPoints == 100) {
+        "is in excellent condition!"
     } else if (healthPoints >= 90) {
-        println("$name has a few scratches")
+        "has a few scratches"
     } else if (healthPoints >= 75) {
         if (isBlessed) {
-            println("$name has some minor wounds but is healing quite quickly!")
+            "has some minor wounds but is healing quite quickly!"
         } else {
-            println("$name has some minor wounds.")
+            "has some minor wounds."
         }
     } else if (healthPoints >= 15) {
-        println("$name looks pretty hurt.")
+        "looks pretty hurt."
     } else {
-        println("$name is in awful condition!")
+        "is in awful condition!"
     }
+
+    println("$name $healthStatus")
 }
