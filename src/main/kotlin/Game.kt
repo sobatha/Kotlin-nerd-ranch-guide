a5186c0262d978880b2bb791ceffb3e126443158
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-03-01 21:54:56 +0900

    when節の利用

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -2,22 +2,25 @@ fun main() {
     val name = "Madrial"
     var healthPoints = 89
     val isBlessed = true
+    val isImmortal = false
 
-    val healthStatus = if (healthPoints == 100) {
-        "is in excellent condition!"
-    } else if (healthPoints >= 90) {
-        "has a few scratches"
-    } else if (healthPoints >= 75) {
-        if (isBlessed) {
-            "has some minor wounds but is healing quite quickly!"
-        } else {
-            "has some minor wounds."
-        }
-    } else if (healthPoints >= 15) {
-        "looks pretty hurt."
-    } else {
-        "is in awful condition!"
+    //Aura
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+
+    val healthStatus = when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches"
+        in 75..89 ->
+            if (isBlessed) {
+                "has some minor wounds but is healing quite quickly!"
+            } else {
+                "has some minor wounds."
+            }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
     }
 
+    println("(Aura: $auraColor) " + "(Blessed: ${if (isBlessed) "YES" else "No"})")
     println("$name $healthStatus")
 }
