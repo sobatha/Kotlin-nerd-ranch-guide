fe6ab471457bb18a2e16d7735eeef58e2fb1baf9
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-03-01 22:53:06 +0900

    単一式関数に

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -10,6 +10,8 @@ fun main() {
     val healthStatus = formatHealthStatus(healthPoints, isBlessed)
 
     printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+
+    castFireball()
 }
 
 private fun printPlayerStatus(
@@ -22,14 +24,12 @@ private fun printPlayerStatus(
     println("$name $healthStatus")
 }
 
-private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
-    return auraColor
-}
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String =
+    if (isBlessed && healthPoints > 50 || isImmortal) "GREEN" else "NONE"
 
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
-    val healthStatus = when (healthPoints) {
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean):String =
+    when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches"
         in 75..89 ->
@@ -38,9 +38,11 @@ private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
             } else {
                 "has some minor wounds."
             }
-
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
     }
-    return healthStatus
+
+
+private fun castFireball(number: Int = 2) {
+    println("A glass of Fireball springs into existence. (x$number)")
 }
