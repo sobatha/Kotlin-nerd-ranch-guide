4b7a66b3ad62510467900fc6122e150f0330096d
sobatha <natsuki.yokoizo.ny@gmail.com>
2024-03-28 21:49:30 +0900

    11章 map練習

--- src/main/kotlin/Taven.kt
+++ src/main/kotlin/Taven.kt
@@ -1,43 +1,48 @@
 import kotlin.math.roundToInt
+import java.io.File
 
 val TAVERN_NAME = "Taernyl's Folly"
-var playerGold = 10
-var playerSilver = 10
+
+val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
+val lastName = listOf("Ironfoot", "Fernsworth", "Baggins")
+val uniquePatrons = mutableSetOf<String>()
+val menuList = File("data/tavern-menu-items.txt").readText().split("\n")
+val patronGold = mutableMapOf<String, Double>()
 fun main() {
-    placeOrder("shady,Dragon's Breath,5.91")
+    (0..9).forEach {
+        val first = patronList.shuffled().first()
+        val last = lastName.shuffled().first()
+        val name = "$first $last"
+        uniquePatrons += name
+    }
+    uniquePatrons.forEach {
+        patronGold[it] = 6.0
+    }
+
+    var orderCount = 0
+    while (orderCount <= 9) {
+        placeOrder(uniquePatrons.shuffled().first(), menuList.shuffled().first())
+        orderCount++
+    }
+
+    displayPatronBalances()
 }
 
-fun performPurchase(price: Double) {
-    displayBalance()
-    val total = playerGold + playerSilver / 100.0
+fun performPurchase(price: Double, patronName: String) {
+    val total = patronGold.getValue(patronName)
+    patronGold[patronName] = total - price
 
-    println("Purchasing item for $price")
-    println("Total purse: $total")
-
-    val remaining = total - price
-    println("Remaining balance: ${"%.2f".format(remaining)}")
-
-    val remainingGold = remaining.toInt()
-    val remainingSilver = (remaining % 1 * 100).roundToInt()
-    playerGold = remainingGold
-    playerSilver = remainingSilver
-    displayBalance()
 }
-
-private fun displayBalance() {
-    println("Player's purse balance: Gold: $playerGold , Silver: $playerSilver")
-}
-
-private fun placeOrder(menuData: String) {
+private fun placeOrder(patronName: String, menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
-    println("Madrigal speaks $tavernMaster about their order.")
+    println("$patronName speaks $tavernMaster about their order.")
 
     val (type, name, price) = menuData.split(",")
-    val message = "Madrigal buys a $name ($type) for $price"
+    val message = "$patronName buys a $name ($type) for $price"
     println(message)
 
-    performPurchase(price.toDouble())
+    performPurchase(price.toDouble(), patronName)
 
     var phrase = "Ah, Delicious $name!"
     println("Madrigal exclaims: ${toDragonSpeak(phrase)}")
@@ -54,4 +59,9 @@ private fun toDragonSpeak(phrase: String) =
             else -> it.value
         }
     }
+private fun displayPatronBalances() {
+    patronGold.forEach { (patron, balance) ->
+        println("$patron, balance: ${"%.2f".format(balance)}")
+    }
+}
 
